1925349902ea29cb3e125fd3bc21b91e18a25552
Kremery <kremery@ukr.net>
2018-12-26 19:46:00 +0200

    this code returns the name of the first country

---
+++ country.py
@@ -0,0 +1,44 @@
+"""
+Country names from the site http://example.webscraping.com
+"""
+
+
+from urllib.request import urlopen, Request
+i = 0
+ACCU_URL = "http://example.webscraping.com/"
+
+response = urlopen(ACCU_URL).read()
+#response = response.decode('utf-8')
+response = str(response)
+#end = '</html>'
+country_tag = 'png" />'
+
+country_tag_size = len(country_tag)
+country_tag_index = response.find(country_tag)
+print("Index: ", country_tag_index)
+country_value_start = country_tag_index + country_tag_size
+print("Start: ", country_value_start)
+country = ''
+
+
+while i == 0:
+	for char in response[country_value_start:]:
+		if char != '<':
+			country += char
+		elif char == '<':
+			country_tag_index = response.find(country_tag, country_value_start)
+			country_value_start = country_tag_index + country_tag_size
+			country += '\n'
+			break
+		else: i = 1
+		
+		
+
+
+#print("Size: ", country_tag_size)
+print("Start: ", country_value_start)
+print("Index: ", country_tag_index)
+#print("country_tag: ", country_tag)
+
+print("Contry: \n", country)
+
